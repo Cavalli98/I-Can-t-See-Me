9a2a3327f82b508e76378105bd0242acc4c3f2d6
TSommariva <48064138+TSommariva@users.noreply.github.com>
2021-12-02 20:08:40 +0100

    Lever e bordi colliders

--- ICantSeeMe/Assets/Scripts/Lever.cs
+++ ICantSeeMe/Assets/Scripts/Lever.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
+    public Sprite activated;
+    public Sprite deactivated;
+    private SpriteRenderer state;
+    private bool isColliding = false;
+    private bool isActive = false;
+
+    private void Awake()
+    {
+        state = GetComponent<SpriteRenderer>();
+        isActive = false;
+        isColliding = false;
+        state.sprite = deactivated;
+    }
     // Start is called before the first frame update
     void Start()
     {
+    
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isColliding )
+        {
+            if (Input.GetKeyDown(KeyCode.E) && !isActive)
+            {
+                GetComponent<SpriteRenderer>().sprite = activated;
+                isActive = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.E) && isActive)
+            {
+                GetComponent<SpriteRenderer>().sprite = deactivated;
+                isActive = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D (Collider2D Player)
+    {        
+        if (Player.tag != "Player")
+            return;
+        isColliding = true;
+    }
+    private void OnTriggerExit2D(Collider2D Player)
+    {
+        if (Player.tag != "Player")
+            return;
+        isColliding = false;
     }
 }
