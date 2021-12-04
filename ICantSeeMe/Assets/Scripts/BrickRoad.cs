5953a77bfd484c4630d07fc1f970cc39026f34b5
cesarm4 <cesarmiguel.martins@mail.polimi.it>
2021-12-04 01:00:56 +0100

    Update BrickRoad.cs

--- ICantSeeMe/Assets/Scripts/BrickRoad.cs
+++ ICantSeeMe/Assets/Scripts/BrickRoad.cs
@@ -8,18 +8,48 @@ public class BrickRoad : Triggerable
     public bool isUp;
     public float startY;
     public float endY;
+    public float speed;
     private float _diff;
+    private bool _isMoving;
+    private float step;
+    private Vector3 endPosition;
+    private Vector3 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        _diff = Mathf.Abs(startY - endY);
+        startPosition = new Vector3(transform.position.x, startY);
+        endPosition = new Vector3(transform.position.x, endY);
+        step = speed * Time.deltaTime; // calculate distance to move
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_isMoving)
+        {
+            // Se isUp prima era true
+            if (!isUp)
+            {
+                // Move our position a step closer to the target.
+                transform.position = Vector3.MoveTowards(transform.position, startPosition, step);
 
+                if (Vector3.Distance(transform.position, startPosition) < 0.01f)
+                {
+                    _isMoving = false;
+                }
+            }
+            else if (isUp)
+            {
+                // Move our position a step closer to the target.
+                transform.position = Vector3.MoveTowards(transform.position, endPosition, step);
+
+                if (Vector3.Distance(transform.position, endPosition) < 0.01f)
+                {
+                    _isMoving = false;
+                }
+            }
+        }
     }
 
     [PunRPC]
@@ -27,13 +57,12 @@ public class BrickRoad : Triggerable
     {
         if (isUp)
         {
-            transform.Translate(0f, -_diff * Time.deltaTime, 0f);
             isUp = false;
         }
         else if (!isUp)
         {
-            transform.Translate(0f, _diff * Time.deltaTime, 0f);
             isUp = true;
         }
+        _isMoving = true;
     }
 }
