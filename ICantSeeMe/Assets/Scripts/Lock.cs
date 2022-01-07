31c9ce680e0ebc2d0448f0d4c575ab2b893e8e41
cesarm4 <cesarmiguel.martins@mail.polimi.it>
2022-01-08 00:29:17 +0100

    Lock prefab

--- ICantSeeMe/Assets/Scripts/Lock.cs
+++ ICantSeeMe/Assets/Scripts/Lock.cs
@@ -1,17 +1,73 @@
 using Photon.Pun;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Lock : Trigger
 {
-    public int hours;
-    public int minutes;
     public GameObject LockPanel;
-    private int code;
-    
+    public int code;
+    List<LockButton> buttons;
+    private bool isActive;
+    private bool locked;
+
     private void Awake()
     {
-        code = int.Parse(hours.ToString() + minutes.ToString());
+        isActive = false;
+    }
+
+    private void Start()
+    {
+        Transform lockZoom = LockPanel.transform.GetChild(0);
+        // Number of buttons
+        int count = lockZoom.childCount;
+
+        buttons = new List<LockButton>();
+        for (int i = 0; i < count; i++)
+        {
+            buttons.Add(lockZoom.GetChild(i).GetComponent<LockButton>());
+        }
+
+        //Set panel for player 1
+        if (PhotonNetwork.IsMasterClient)
+        {
+            // Deactivate all text components
+            foreach (LockButton button in buttons)
+            {
+                button.textUI.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            //Deactivate all buttons
+            foreach (LockButton button in buttons)
+            {
+                button.buttonUI.interactable = false;
+            }
+        }
+    }
+
+    public void UpdateCode()
+    {
+        int number = 0;
+        foreach (LockButton button in buttons)
+        {
+            number *= 10;
+            number += button.number;
+        }
+        
+        if (number == code)
+        {
+            LockPanel.SetActive(false);
+            foreach (GameObject t in toActivate)
+                t.GetComponent<Triggerable>().activate();
+            Destroy(gameObject);
+        }
+    }
+
+    [PunRPC]
+    public override void trigger()
+    {
+        isActive = !isActive;
+        LockPanel.SetActive(isActive);
     }
 }
---
+++ ICantSeeMe/Assets/Scripts/LockButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class LockButton : MonoBehaviourPun
+{
+    public Text textUI;
+    public UnityEngine.UI.Button buttonUI;
+    public int number;
+    public Lock lockObj;
+
+    private void Awake()
+    {
+        buttonUI = GetComponent<UnityEngine.UI.Button>();
+        number = 0;
+    }
+
+    public void OnClick()
+    {
+        photonView.RPC("UpdateNumber", RpcTarget.All, null);
+    }
+
+    [PunRPC]
+    public void UpdateNumber()
+    {
+        number = (number + 1) % 10;
+        textUI.text = number.ToString();
+        lockObj.UpdateCode();
+    }
+}
