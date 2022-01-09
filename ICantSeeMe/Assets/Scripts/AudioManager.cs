c4d2ffc422747a2555434ded00fdb05445de3031
cesarm4 <cesarmiguel.martins@mail.polimi.it>
2022-01-09 03:42:15 +0100

    test

--- ICantSeeMe/Assets/Scripts/AudioManager.cs
+++ ICantSeeMe/Assets/Scripts/AudioManager.cs
@@ -34,6 +34,7 @@ public class Sound
     {
         source.volume = volume; /** (1 + Random.Range(-randomVolume / 2f, randomVolume / 2f));*/
         //source.pitch = pitch * (1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
+        source.loop = false;
         source.Play();
     }
 
